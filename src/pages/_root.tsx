9939e509f6ba91054c72d92a6edbf7404155b8b4
ergofriend <ergofriend@kasu.dev>
2026-08-30 02:27:42 +0900

    chore: scaffold Waku Cloudflare app

---
+++ src/pages/_root.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from 'react';
+
+export default function Root({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>whoami</title>
+        <meta
+          name="description"
+          content="Check your IP address, network, browser, and request information."
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
